fd0e7d60055665d8cb390f47a1e08c6dc6f16e91
Kevin Saiger <kevin@Kevins-iMac.local>
2019-12-14 21:16:59 +0100

    Update install scripts

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,3 +1,3 @@
 sudo wget https://github.com/csskevin/web-glasses/releases/download/v1.0/web-glasses-server_armv7 --directory-prefix=/bin/
 sudo wget https://raw.githubusercontent.com/csskevin/web-glasses/master/scripts/linux/webglasses.service --directory-prefix=/etc/systemd/system
-systemctl enable webglasses.service
+sudo systemctl enable webglasses.service

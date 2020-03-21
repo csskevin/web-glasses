d402bfc029519a0950e72961a7c95a1a66da8c9f
csskevin <saigerkevin4@gmail.com>
2020-03-21 21:43:35 +0100

    Removing chromium.

--- dist/src/chromium.d.ts
+++
@@ -1,4 +0,0 @@
-declare const run_chromium: () => Promise<void>;
-declare const stop_chromium: () => void;
-declare const restart_chromium: () => Promise<void>;
-export { run_chromium, stop_chromium, restart_chromium };
